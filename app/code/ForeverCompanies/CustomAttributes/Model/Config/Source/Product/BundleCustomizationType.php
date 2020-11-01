879dfe811a1b349c9af5efc8d6721abbbf686c3f
Steve Caponetto <steve.caponetto@forevercompanies.com>
2020-11-01 14:31:50 -0600

    add mapping of option title to option

--- app/code/ForeverCompanies/CustomAttributes/Model/Config/Source/Product/BundleCustomizationType.php
+++ app/code/ForeverCompanies/CustomAttributes/Model/Config/Source/Product/BundleCustomizationType.php
@@ -10,7 +10,14 @@ class BundleCustomizationType extends AbstractCustomizationType
     const OPTIONS = [
         ' ' => '',
         'Matching Band' => 'matching_band',
-        'Matching Bands' => 'matching_band',
         'Center Stone Size' => 'center_stone_size'
     ];
+
+    const TITLE_MAPPING = [
+        '' => '',
+        'Matching Band' => 'Matching Band',
+        'Matching Bands' => 'Matching Band',
+        'Center Stone Size' => 'Center Stone Size',
+    ];
+
 }
