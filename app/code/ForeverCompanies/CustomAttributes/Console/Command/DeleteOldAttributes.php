be9ef8cc8eb6c18690745f6215ba2839485ed3b7
veroslayev <valeriy.eroslayev@forevercompanies.com>
2020-11-17 17:01:46 +0300

    [BUG: Additional Images Missing][BUG: Associated products not disabled]

--- app/code/ForeverCompanies/CustomAttributes/Console/Command/DeleteOldAttributes.php
+++ app/code/ForeverCompanies/CustomAttributes/Console/Command/DeleteOldAttributes.php
@@ -21,7 +21,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class DeleteOIdAttributes extends AbstractCommand
+class DeleteOldAttributes extends AbstractCommand
 {
 
     /**
