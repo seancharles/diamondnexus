334fb962a02a82f3889dfa224d334a46812f7865
Paul Baum <paul.baum@forevercompanies.com>
2020-12-09 15:30:59 -0600

    Saving progress

--- app/code/ForeverCompanies/Forms/Controller/Post/Index.php
+++ app/code/ForeverCompanies/Forms/Controller/Post/Index.php
@@ -2,16 +2,36 @@
 
 	namespace ForeverCompanies\Forms\Controller\Post;
 
+	use ForeverCompanies\Forms\Model\DataExampleFactory;
+	use Magento\Framework\Controller\ResultFactory;
+	use Magento\Framework\App\Action\Context;
+
 	class Index extends \Magento\Framework\App\Action\Action
 	{
+		protected $submissionFactory;
+		protected $resultFactory;
+		
 		public function __construct(
-			\Magento\Backend\App\Action\Context $context
+			\Magento\Framework\App\Action\Context $context,
+			\ForeverCompanies\Forms\Model\SubmissionFactory  $submissionFactory,
+			\Magento\Framework\Controller\ResultFactory $resultFactory
 		) {
 			parent::__construct($context);
+			
+			$this->submissionFactory = $submissionFactory;
+			$this->resultFactory = $resultFactory;
 		}
 
 		public function execute()
 		{
 			echo "Hello World";
+			
+			$model = $this->submissionFactory->create();
+			$model->addData([
+				"store_id" => '1',
+				"form_post_json" => json_encode(['key'=>'hello world']),
+				"created_at" => date("Y-m-d h:i:s", time())
+			]);
+			$saveData = $model->save();
 		}
 	}
---
+++ app/code/ForeverCompanies/Forms/Model/Submission.php
@@ -0,0 +1,8 @@
+<?php 
+namespace ForeverCompanies\Forms\Model;
+class Submission extends \Magento\Framework\Model\AbstractModel{
+	public function _construct(){
+		$this->_init("ForeverCompanies\Forms\Model\ResourceModel\Submission");
+	}
+}
+ ?>
