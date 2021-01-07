67a55dad1e0c9ff66b47d4e4284889c460093671
Paul Baum <paul.baum@forevercompanies.com>
2021-01-06 19:42:28 -0600

    Adding sales person to order grid

---
+++ app/code/ForeverCompanies/CustomSales/Ui/Component/Columns/SalesPerson.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ForeverCompanies\CustomSales\Ui\Component\Columns;
+
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\User\Model\UserFactory;
+
+class SalesPerson extends Column
+{
+    /**
+     * Constructor
+     *
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        OrderRepositoryInterface $orderRepository,
+        UserFactory $userFactory,
+        array $components = [],
+        array $data = []
+    )
+    {
+        $this->_orderRepository = $orderRepository;
+        $this->userFactory = $userFactory;
+        
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+
+                // load the user
+                $user = $this->userFactory->create()->load($item["sales_person_id"]);
+                
+                $item[$this->getData('name')] = $user->getUsername();
+            }
+        }
+
+        return $dataSource;
+    }
+}
