13b213d4d66096a0c6498a2031cebc9388f9d2cc
Steve Caponetto <steve.caponetto@forevercompanies.com>
2021-01-22 08:59:54 -0600

    if price null log error and move on

--- app/code/ForeverCompanies/CustomAttributes/Helper/TransformData.php
+++ app/code/ForeverCompanies/CustomAttributes/Helper/TransformData.php
@@ -864,7 +864,7 @@ class TransformData extends AbstractHelper
             return;
         }
         if ($product->getName() == null) {
-            $this->_logger->error('Product ID = ' . $entityId . ' without name');
+            $this->_logger->error('updateBundlePriceType Error: Product ID = ' . $entityId . ' without name');
             return;
         }
 
@@ -874,27 +874,26 @@ class TransformData extends AbstractHelper
             return;
         }
 
-        // check if a price exists, if not, add a record
+        // check if a price exists, if not, throw error
         if (is_null($product->getPrice())) {
-            $product->setPrice(0.00);
-            $this->saveProduct($product);
-            $product = $this->getCurrentProduct($entityId);
+            $this->_logger->error('updateBundlePriceType Error: Product ID = ' . $entityId . ' has no price attribute record.');
+            return;
         }
+
+        // should we save the product record?
+        $productSave = false;
 
         // check if the price_type attribute is set 1. if not, set it.
         if (Price::PRICE_TYPE_FIXED !== (int) $product->getData('price_type')) {
             $product->setData('price_type', Price::PRICE_TYPE_FIXED);
-            $this->saveProduct($product);
+            $productSave = true;
         }
-    }
 
-    /**
-     * @param $product
-     * @throws InputException
-     * @throws StateException
-     */
-    private function saveProduct($product)
-    {
+        // don't call product->save if nothing needs saving
+        if (!$productSave) {
+            return;
+        }
+
         // save the product
         try {
             $this->productRepository->save($product);
