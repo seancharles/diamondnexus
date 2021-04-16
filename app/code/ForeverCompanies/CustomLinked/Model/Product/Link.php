f32317df956400a5e04b69ad0ad1060c54ba0c5d
Paul Baum <paul.baum@forevercompanies.com>
2021-04-16 12:37:44 -0500

    Updating matching bands, enhancers module to use magepal version

--- app/code/ForeverCompanies/CustomLinked/Model/Product/Link.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace ForeverCompanies\CustomLinked\Model\Product;
-class Link extends \Magento\Catalog\Model\Product\Link
-{
-    const LINK_TYPE_CUSTOMLINKED = 17;
-}
---
+++ app/code/ForeverCompanies/LinkProduct/Model/Accessory.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Copyright © ForeverCompanies LLC. All rights reserved.
+ * See COPYING.txt for license details.
+ * http://www.forevercompanies.com | support@forevercompanies.com
+ */
+
+namespace ForeverCompanies\LinkProduct\Model;
+
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ResourceModel\Product\Link\Collection;
+use Magento\Framework\DataObject;
+use ForeverCompanies\LinkProduct\Model\Product\Link;
+
+class Accessory extends DataObject
+{
+    /**
+     * Product link instance
+     *
+     * @var Product\Link
+     */
+    protected $linkInstance;
+
+    /**
+     * Accessory constructor.
+     * @param Link $productLink
+     */
+    public function __construct(
+        Link $productLink
+    ) {
+        $this->linkInstance = $productLink;
+    }
+
+    /**
+     * Retrieve link instance
+     *
+     * @return  Product\Link
+     */
+    public function getLinkInstance()
+    {
+        return $this->linkInstance;
+    }
+
+    /**
+     * Retrieve array of Accessory products
+     *
+     * @param Product $currentProduct
+     * @return array
+     */
+    public function getAccessoryProducts(Product $currentProduct)
+    {
+        if (!$this->hasAccessoryProducts()) {
+            $products = [];
+            $collection = $this->getAccessoryProductCollection($currentProduct);
+            foreach ($collection as $product) {
+                $products[] = $product;
+            }
+            $this->setAccessoryProducts($products);
+        }
+        return $this->getData('accessory_products');
+    }
+
+    /**
+     * Retrieve accessory products identifiers
+     *
+     * @param Product $currentProduct
+     * @return array
+     */
+    public function getAccessoryProductIds(Product $currentProduct)
+    {
+        if (!$this->hasAccessoryProductIds()) {
+            $ids = [];
+            foreach ($this->getAccessoryProducts($currentProduct) as $product) {
+                $ids[] = $product->getId();
+            }
+            $this->setAccessoryProductIds($ids);
+        }
+        return $this->getData('accessory_product_ids');
+    }
+
+    /**
+     * Retrieve collection accessory product
+     *
+     * @param Product $currentProduct
+     * @return \Magento\Catalog\Model\ResourceModel\Product\Link\Product\Collection
+     */
+    public function getAccessoryProductCollection(Product $currentProduct)
+    {
+        $collection = $this->getLinkInstance()->useAccessoryLinks()->getProductCollection()->setIsStrongMode();
+        $collection->setProduct($currentProduct);
+        $collection->addAttributeToSelect('name','catalog_product_entity_varchar');
+        return $collection;
+    }
+
+    /**
+     * Retrieve collection accessory link
+     *
+     * @param Product $currentProduct
+     * @return Collection
+     */
+    public function getAccessoryLinkCollection(Product $currentProduct)
+    {
+        $collection = $this->getLinkInstance()->useAccessoryLinks()->getLinkCollection();
+        $collection->setProduct($currentProduct);
+        $collection->addLinkTypeIdFilter();
+        $collection->addProductIdFilter();
+        $collection->joinAttributes();
+        return $collection;
+    }
+}
