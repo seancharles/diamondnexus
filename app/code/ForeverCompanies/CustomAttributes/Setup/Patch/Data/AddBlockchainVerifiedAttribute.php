f1146eeca6f8340543c00cc299ca7bd4bd9abfd0
Zach Bailey <zach.bailey@forevercompanies.com>
2021-04-19 16:05:04 -0500

    BE: Product Attribute Fixes

---
+++ app/code/ForeverCompanies/CustomAttributes/Setup/Patch/Data/AddBlockchainVerifiedAttribute.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace ForeverCompanies\CustomAttributes\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Type;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Zend_Validate_Exception;
+
+class AddBlockchainVerifiedAttribute implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws LocalizedException
+     * @throws Zend_Validate_Exception
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $attribute = $eavSetup->getAttribute(Product::ENTITY, 'blockchain_verified');
+        if ($attribute) {
+            $eavSetup->removeAttribute(
+                Product::ENTITY,
+                'blockchain_verified'
+            );
+        }
+        
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'blockchain_verified',
+            [
+                'type' => 'varchar',
+                'label' => 'Blockchain Verified',
+                'input' => 'text',
+                'source' => '',
+                'frontend' => '',
+                'required' => false,
+                'backend' => '',
+                'sort_order' => '50',
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'default' => null,
+                'visible' => true,
+                'user_defined' => true,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'unique' => false,
+                'apply_to' => Type::TYPE_SIMPLE,
+                'group' => 'General',
+                'used_in_product_listing' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+            ]
+        );
+        
+        $eavSetup->addAttributeToGroup(
+            Product::ENTITY,
+            'Migration_Loose Diamonds',
+            'General', 
+            'blockchain_verified',
+        );
+            
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->removeAttribute(Product::ENTITY, 'blockchain_verified');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getVersion()
+    {
+        return '0.0.8';
+    }
+}
