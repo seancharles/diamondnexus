ec2e189139b87194078d5fa267ab3a7f77bc197e
Steve Caponetto <steve.caponetto@forevercompanies.com>
2021-04-29 14:38:58 -0500

    set used_in_product_listing for video_url attribute

---
+++ app/code/ForeverCompanies/CustomAttributes/Setup/Patch/Data/UpdateVideoUrlProductListing.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ForeverCompanies\CustomAttributes\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+
+class UpdateVideoUrlProductListing implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @var string
+     */
+    private $_attribute = 'video_url';
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->updateAttribute(
+            Product::ENTITY,
+            $this->_attribute,
+            'used_in_product_listing',
+            1
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        // change url_key to be set to searchable (is_searchable = 0)
+        $eavSetup->updateAttribute(
+            Product::ENTITY,
+            $this->_attribute,
+            'used_in_product_listing',
+            0
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
