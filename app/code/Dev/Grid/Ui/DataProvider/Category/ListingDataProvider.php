e6c771c07d5927f87772e4e47a2eab1345b28dc5
Zach Bailey <zach.bailey@forevercompanies.com>
2021-04-30 17:21:16 +0000

    there is a grid!

--- app/code/Dev/Grid/Ui/DataProvider/Category/ListingDataProvider.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Dev\Grid\Ui\DataProvider\Category;
-
-
-class ListingDataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
-{
-}
--- app/code/RH/CustProductGrid/registration.php
+++ app/code/RH/CustProductGrid/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-   'Dev_Grid',
+   'RH_CustProductGrid',
    __DIR__
 );
