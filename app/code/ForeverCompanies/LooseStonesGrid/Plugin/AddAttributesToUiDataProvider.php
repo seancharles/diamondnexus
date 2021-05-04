51a6fdab0e6ccbf2b1af10cda69811dd341f9b17
Zach Bailey <zach.bailey@forevercompanies.com>
2021-05-04 04:51:21 +0000

    most attributes are showing in the grid

--- app/code/ForeverCompanies/LooseStonesGrid/Plugin/AddAttributesToUiDataProvider.php
+++ app/code/ForeverCompanies/LooseStonesGrid/Plugin/AddAttributesToUiDataProvider.php
@@ -50,16 +50,82 @@ class AddAttributesToUiDataProvider
             $column = 'row_id';
         }
         
-        $attribute = $this->attributeRepository->get('catalog_product', 'name');
+        $attributeArr = array(
+            "supplier",
+            "filter_ship_date",
+            "rapaport",
+            "pct_off_rap",
+            
+            "price",
+            // "custom_price",
+            
+            "cost",
+            "custom_cost",
+            
+            "cert_url_key",
+            "diamond_img_url",
+            "video_url",
+            "online",
+            "lab",
+            "shape",
+            "color",
+            "clarity",
+            "cut_grade",
+            "stone_carat",
+            "country_of_manufacture",
+            "length_to_width",
+            
+            "measurements",
+            "polish",
+            "symmetry",
+            "girdle",
+            "fluor",
+            "as_grown",
+            "born_on_date",
+            "carbon_neutral",
+            "blockchain_verified",
+            "charitable_contribution",
+            "cvd",
+            "hpht",
+            "patented",
+            "custom",
+            "color_of_colored_diamonds",
+            "hue",
+            "intensity"
+        );
+        
+        foreach ($attributeArr as $attr) {
+            try {
+                $attribute = $this->attributeRepository->get('catalog_product', $attr);
+            } catch(\Magento\Framework\Exception\NoSuchEntityException $e) {
+                
+            }
+            
+            if (isset($attribute)) {
+                $result->getSelect()->joinLeft(
+                    ['stonesgrid_' . $attr => $attribute->getBackendTable()],
+                    'stonesgrid_' . $attr . '.' . $column . ' = main_table.' . $column . ' AND stonesgrid_' . $attr . '.attribute_id = '
+                    . $attribute->getAttributeId(),
+                    [$attr => 'stonesgrid_' . $attr . '.value']
+                );
+            }
+            unset($attribute);
+        }
+        
+        
+        /*
+        $attribute = $this->attributeRepository->get('catalog_product', 'supplier');
         
         $result->getSelect()->joinLeft(
-            ['devgridname' => $attribute->getBackendTable()],
-            'devgridname.' . $column . ' = main_table.' . $column . ' AND devgridname.attribute_id = '
+            ['stonesgrid_supplier' => $attribute->getBackendTable()],
+            'stonesgrid_supplier.' . $column . ' = main_table.' . $column . ' AND stonesgrid_supplier.attribute_id = '
             . $attribute->getAttributeId(),
-            ['name' => 'devgridname.value']
-            );
+            ['supplier' => 'stonesgrid_supplier.value']
+        );
+        */
         
-        $result->getSelect()->where('devgridname.value LIKE "B%"');
+    
+    //   $result->getSelect()->where('devgridname.value LIKE "B%"');
         
         return $result;
     }
