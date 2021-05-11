4bb25e78bb49c62e591b2f7c83798dec79a181b2
Zach Bailey <zach.bailey@forevercompanies.com>
2021-05-11 18:55:41 +0000

    loading product coll

--- app/code/ForeverCompanies/LooseStonesQuery/Model/Resolver/StonesQuery.php
+++ app/code/ForeverCompanies/LooseStonesQuery/Model/Resolver/StonesQuery.php
@@ -10,19 +10,24 @@ use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\GraphQl\Query\Resolver\BatchResolverInterface;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 
 
 
 class StonesQuery implements ResolverInterface
 {
+    protected $productColl;
+    
     
     public function __construct(
         \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
+        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder,
+        CollectionFactory $collectionFac
         ) {
         
             $this->productRepository = $productRepository;
             $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+            $this->productColl = $collectionFac->create();
             
     }
     
@@ -33,7 +38,65 @@ class StonesQuery implements ResolverInterface
         ResolveInfo $info,
         array $value = null,
         array $args = null
-        ) {
+        ) { 
+            
+            $onlineEqFilter = $args['filter']['online']['eq'];
+            $productTypeEqFilter = $args['filter']['product_type']['eq'];
+            $colorInFilter = $args['filter']['color']['in'];
+            $clarityInFilter = $args['filter']['clarity']['in'];
+            $caratWeightFromFilter = $args['filter']['carat_weight']['from'];
+            $caratWeightToFilter = $args['filter']['carat_weight']['to'];
+            $priceFromFilter = $args['filter']['price']['from'];
+            $priceToFilter = $args['filter']['price']['to'];
+            $cutGradeInFilter = $args['filter']['cut_grade']['in'];
+            $shapeInFilter = $args['filter']['shape']['in'];
+            $pageSize = $args['pageSize'];
+            $currentPage = $args['currentPage'];
+            
+            foreach ($args['sort'] as $k => $v) {
+                $sortKey = $k;
+                $sortVal = $v;
+            }
+            
+            $this->productColl->addAttributeToFilter('online', $onlineEqFilter);
+            $this->productColl->addAttributeToFilter('product_type', $productTypeEqFilter);
+            $this->productColl->addAttributeToFilter('color', array('in' => $colorInFilter));
+            $this->productColl->addAttributeToFilter('clarity', array('in' =>  $clarityInFilter));
+            $this->productColl->addAttributeToFilter('cut_grade', array('in' =>  $cutGradeInFilter));
+            $this->productColl->addAttributeToFilter('shape', array('in' =>  $shapeInFilter));
+            
+            $this->productColl->addAttributeToFilter('carat_weight', array('gteq' =>  $caratWeightFromFilter));
+            $this->productColl->addAttributeToFilter('carat_weight', array('lteq' =>  $caratWeightToFilter));
+            $this->productColl->addAttributeToFilter('price', array('gteq' =>  $priceFromFilter));
+            $this->productColl->addAttributeToFilter('price', array('lteq' =>  $priceToFilter));
+         
+            $this->productColl->setPageSize($pageSize);
+            $this->productColl->setCurPage($currentPage);
+            
+            $this->productColl->addAttributeToSelect("*")->load();
+            
+            echo 'the product coll count is ' . count($this->productColl);
+            
+            foreach ($this->productColl as $_product) {
+                echo $_product->getName() . "    ";
+            }
+            
+            die;
+            
+            var_dump($sortKey);
+            var_dump($sortVal);
+            die;
+            
+            
+            foreach ($args['filter'] as $argFilter) {
+                echo '<pre>';
+                var_dump($argFilter);
+            }
+            die;
+            echo '<pre>';
+            var_dump($args['filter']['online']['eq']);
+            die;
+            echo $args['filter']['online'];die;
             
             echo '<pre>';
             var_dump("args", $args);
