35115a659909434e20ba6bd2ac37d7d19c854086
Zack <Bailey>
2021-05-20 01:15:26 -0500

    admin orders working

--- app/code/ForeverCompanies/Gifts/Observer/SalesOrderPlaceAfter.php
+++ app/code/ForeverCompanies/Gifts/Observer/SalesOrderPlaceAfter.php
@@ -9,6 +9,8 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Model\QuoteFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
 
 class SalesOrderPlaceAfter implements ObserverInterface
 {
@@ -19,6 +21,36 @@ class SalesOrderPlaceAfter implements ObserverInterface
     protected $purchaseHelper;
     
     protected $quoteFactory;
+    
+    
+    /**
+     * @var \Magento\Sales\Api\OrderRepositoryInterface
+     */
+    protected $orderRepository;
+    
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    protected $productRepository;
+    
+    /**
+     * @var \Magento\Quote\Api\Data\CartItemInterfaceFactory
+     */
+    protected $cartItemFactory;
+    
+    /**
+     * @var \Magento\Quote\Api\CartRepositoryInterface
+     */
+    protected $quoteRepository;
+    
+    /**
+     * @var \Magento\Sales\Model\Order\ItemFactory
+     */
+    protected $orderItemFactory;
+    
+    protected $scopeConfig;
+    protected $storeScope;
+    
 
     /**
      * ProcessQuoteObserver constructor.
@@ -27,14 +59,31 @@ class SalesOrderPlaceAfter implements ObserverInterface
     public function __construct(
         FreeGift $giftH,
         Purchase $purchaseH,
-        QuoteFactory $quoteF
+        QuoteFactory $quoteF,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Quote\Api\Data\CartItemInterfaceFactory $cartItemFactory,
+        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
+        \Magento\Sales\Model\Order\ItemFactory $orderItemFactory,
+        ScopeConfigInterface $scopeConfig
     ) {
+        
         $this->giftHelper = $giftH;
         $this->purchaseHelper = $purchaseH;
         
         $this->quoteFactory = $quoteF;
+        
+        
+        $this->productRepository    = $productRepository;
+        $this->orderRepository      = $orderRepository;
+        $this->cartItemFactory      = $cartItemFactory;
+        $this->quoteRepository      = $quoteRepository;
+        $this->orderItemFactory     = $orderItemFactory;
+        
+        $this->scopeConfig = $scopeConfig;
+        $this->storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
     }
-
+    
     /**
      * Used before quote recollect
      *
@@ -44,45 +93,78 @@ class SalesOrderPlaceAfter implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/giftlog.log');
-        $logger = new \Zend\Log\Logger();
-        $logger->addWriter($writer);
-        $logger->info( 'observer name - ' . $observer->getEvent()->getName() );
-        $logger->info( 'order id - ' . $observer->getEvent()->getOrder()->getId() );
-        
-        $eventName = $observer->getEvent()->getName(); 
-        
-        
-        $freeGift = false;
-        if ($this->giftHelper->isEnabledFreeGift()) {
-            
-            $logger->info('free gift');
-            
-            $quote = $this->quoteFactory->create()->load($observer->getEvent()->getOrder()->getId());
-            
-            
-            $giftSkus = $this->giftHelper->fillGifts($quote);
-            
-            foreach ($giftSkus as $sku => $qty) {
-                $freeGift = true;
-                $this->giftHelper->addGiftToQuote($quote, $sku, $qty, false, true, $observer->getEvent()->getName() );
-            }
-        }
-
-        if (!$freeGift && $this->purchaseHelper->isEnabledPurchase()) {
-            
-            $logger->info('purchase');
-            
-            $quote = $this->quoteFactory->create()->load($observer->getEvent()->getOrder()->getId());
-            
-            $logger->info( 'quote id - ' . $quote->getId() );
-      
-            
-            $this->purchaseHelper->addGiftToQuote($quote);
-        } 
-        
+        $this->_addFreeItemToOrder($observer->getEvent()->getOrder());
         return;
     }
     
+    protected function _addFreeItemToOrder($order)
+    {
+        $quote = $this->quoteRepository->get($order->getQuoteId());
+        $freeItem = false;
+        $giftSkus = $this->giftHelper->fillGifts($quote);
+            
+        if (count($giftSkus) > 0) {
+            $freeItem = true;
+            foreach ($giftSkus as $sku => $qty) {
+                   $freeSku = $sku;
+                   break;
+            }
+        }
+            
+        if(!$freeItem) {
+            foreach ($quote->getAllItems() as $_quoteItem) {
+                
+                $_quoteItem->setSetId(42);
+                
+                if ($_quoteItem->getSetId() > 0) {
+                    if ($quote->getSubtotal() >= $this->scopeConfig->getValue('forevercompanies_gifts/purchase/total', $this->storeScope)) {
+                        $freeItem = true;
+                        $freeSku = $this->scopeConfig->getValue('forevercompanies_gifts/purchase/product_id', $this->storeScope);
+                        break;
+                    }
+                }   
+            }
+        }
+        
+        if (!$freeItem) {
+            return;
+        }
+        
+        $product = $this->productRepository->get($freeSku);
+        
+        $quoteItem = $this->cartItemFactory->create();
+        $quoteItem->setProduct($product);
+        $quoteItem->setCustomPrice(0);
+        $quoteItem->setOriginalCustomPrice($product->getPrice());
+        $quoteItem->getProduct()->setIsSuperMode(true);
+        $quote->addItem($quoteItem);
+        $quote->save();
+        
+        $orderItem = $this->orderItemFactory->create();
+        $orderItem
+        ->setStoreId($order->getStoreId())
+        ->setQuoteItemId($quoteItem->getId())
+        ->setProductId($product->getId())
+        ->setProductType($product->getTypeId())
+        ->setName($product->getName())
+        ->setSku($product->getSku())
+        ->setQtyOrdered(1)
+        ->setPrice(0)
+        ->setBasePrice(0)
+        ->setOriginalPrice($product->getPrice())
+        ->setBaseOriginalPrice($product->getPrice())
+        ->setPriceInclTax(0)
+        ->setBasePriceInclTax(0)
+        ->setRowTotal(0)
+        ->setBaseRowTotal(0)
+        ->setRowTotalInclTax(0)
+        ->setBaseRowTotalInclTax(0)
+        ->setWeight(1)
+        ->setIsVirtual(1);
+        $order->addItem($orderItem);
+        
+        $order->setTotalItemCount($order->getTotalItemCount() + 1);
+        $order->setTotalQtyOrdered($order->getTotalQtyOrdered() + 1);
+        $this->orderRepository->save($order);
+    }
 }
