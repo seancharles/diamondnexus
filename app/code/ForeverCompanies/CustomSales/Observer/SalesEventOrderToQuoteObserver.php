9477abc96e2cd09702eb919b825f9caed47e50d3
Zack <Bailey>
2021-05-25 11:45:57 -0500

    adding to push

--- app/code/ForeverCompanies/CustomSales/Observer/SalesEventOrderToQuoteObserver.php
+++ app/code/ForeverCompanies/CustomSales/Observer/SalesEventOrderToQuoteObserver.php
@@ -16,10 +16,40 @@ class SalesEventOrderToQuoteObserver implements ObserverInterface
     {
         /** @var Order $order */
         $order = $observer->getData('order');
-        if ($order->getData('reordered')) {
+       // if (1==0 && $order->getData('reordered')) {
+       
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/reordertest.log');
+        $logger = new \Zend\Log\Logger();
+        $logger->addWriter($writer);
+        
+        $logger->info('order get remote ip');
+        $logger->info($order->getRemoteIp());
+        
+        
+        if (trim($order->getRemoteIp()) != "")
+        {
+                
+            
+           
+            $logger->info('order data reordered');
+            $logger->info($order->getData('reordered'));
+            
+            $logger->info('order sales person id');
+            $logger->info($order->getData('sales_person_id'));
+            
             /** @var Quote $quote */
             $quote = $observer->getData('quote');
             $quote->setData('sales_person_id', $order->getData('sales_person_id'));
+            
+            $logger->info('quote sales person id');
+            $logger->info($quote->getData('sales_person_id'));
+            
+            $logger->info('order data keys');
+            $logger->info(array_keys( $order->getData() ));
+            
+           
+       //     $quote->save();
+            
         }
 
         return $this;
