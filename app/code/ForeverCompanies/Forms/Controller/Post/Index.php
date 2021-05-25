84749dacbc917f19d8aac2088742d1c1f72ed401
Zack <Bailey>
2021-05-25 12:19:04 -0500

    added check for honeypot field

--- app/code/ForeverCompanies/Forms/Controller/Post/Index.php
+++ app/code/ForeverCompanies/Forms/Controller/Post/Index.php
@@ -39,42 +39,46 @@
 		public function execute()
 		{
 			if ($this->formKeyValidator->validate($this->getRequest()) == false) {
-			
-				$websiteId = $this->storeManager->getWebsite()->getId();
-				$formId = $this->formHelper->getSanitizedField('form_id');
-                $email = $this->formHelper->getSanitizedField('email');
-                $formData = $this->formHelper->getSanitizedField('form_post_json');
-				
-				$model = $this->submissionFactory->create();
-				
-                $leadKey = $this->cookieManager->getCookie(self::COOKIE_NAME);
-                
-                if(!strlen($leadKey) > 0) {
-                    // generate new submission key (unique to each form)
-                    $leadKey = $websiteId . $formId . bin2hex(random_bytes(20));
-                    
-                     $metadata = $this->cookieMetadataFactory
-                         ->createPublicCookieMetadata()
-                         ->setDuration(self::COOKIE_DURATION);
-                         
-                     $this->cookieManager->setPublicCookie(
-                         self::COOKIE_NAME,
-                         $leadKey,
-                         $metadata
-                     );
-                }
-                
-				$model->addData([
-                    "lead_key" => $leadKey,
-                    "form_id" => $formId,
-					"website_id" => $websiteId,
-                    "email" => $email,
-					"form_post_json" => $formData,
-					"created_at" => date("Y-m-d h:i:s", time())
-				]);
-				
-				$saveData = $model->save();
-				
+		
+			    // if field is populated a bot filled out the honey pot.
+			    if (trim($this->formHelper->getSanitizedField('email_confirm')) == "") {
+			        
+			        $websiteId = $this->storeManager->getWebsite()->getId();
+			        $formId = $this->formHelper->getSanitizedField('form_id');
+			        $email = $this->formHelper->getSanitizedField('email');
+			        $formData = $this->formHelper->getSanitizedField('form_post_json');
+			        
+			        $model = $this->submissionFactory->create();
+			        
+			        $leadKey = $this->cookieManager->getCookie(self::COOKIE_NAME);
+			        
+			        if(!strlen($leadKey) > 0) {
+			            // generate new submission key (unique to each form)
+			            $leadKey = $websiteId . $formId . bin2hex(random_bytes(20));
+			            
+			            $metadata = $this->cookieMetadataFactory
+			            ->createPublicCookieMetadata()
+			            ->setDuration(self::COOKIE_DURATION);
+			            
+			            $this->cookieManager->setPublicCookie(
+			                self::COOKIE_NAME,
+			                $leadKey,
+			                $metadata
+			                );
+			        }
+			        
+			        $model->addData([
+			            "lead_key" => $leadKey,
+			            "form_id" => $formId,
+			            "website_id" => $websiteId,
+			            "email" => $email,
+			            "form_post_json" => $formData,
+			            "created_at" => date("Y-m-d h:i:s", time())
+			        ]);
+			        
+			        $saveData = $model->save();
+			    }
+			    
 				echo json_encode(['success'=>true]);
 				
 			} else {
