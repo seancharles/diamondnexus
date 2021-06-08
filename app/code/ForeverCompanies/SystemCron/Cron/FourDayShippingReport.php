9ded5720969a3600a9a2d9a3e7cdf8efa44088b0
Zack Bailey <zach.bailey@forevercompanies.com>
2021-06-08 15:31:00 -0500

    committing to test dev branch

---
+++ app/code/ForeverCompanies/SystemCron/Cron/FourDayShippingReport.php
@@ -0,0 +1,19 @@
+<?php
+namespace ForeverCompanies\SystemCron\Cron;
+
+
+class FourDayShippingReport
+{
+
+	public function __construct(
+	    
+	) {
+		
+	}
+	
+	public function execute()
+	{
+	    
+	}
+	
+}
