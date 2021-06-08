83b688b2808b75d1b518b434ba3f35ff97ef2a90
Ted Gerstle <ted.gerstle@forevercompanies.com>
2021-06-08 16:46:25 -0500

    fix csp issue

--- app/design/frontend/ForeverCompanies/tf/registration.php
+++ app/design/frontend/ForeverCompanies/tf/registration.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
@@ -7,3 +8,5 @@
 use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/ForeverCompanies/tf', __DIR__);
+//making the theme into a module so that the csp_whitelist.xml gets read
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'ForeverCompanies_tf', __DIR__);
