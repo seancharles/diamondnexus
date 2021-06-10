0f919174eed7bea21890025d3c8d6fac2a787018
Zack Bailey <zach.bailey@forevercompanies.com>
2021-06-10 12:59:49 -0500

    Sale promo discount report

--- app/code/ForeverCompanies/SystemCron/Controller/Index/Index.php
+++ app/code/ForeverCompanies/SystemCron/Controller/Index/Index.php
@@ -13,6 +13,11 @@ use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Store\Model\StoreManagerInterface;
 
+use Magento\Sales\Model\ResourceModel\Order\Shipment\Collection as OrderShipmentCollection;
+use Magento\Catalog\Model\ProductFactory;
+use Magento\Sales\Model\OrderFactory;
+use Magento\Store\Api\WebsiteRepositoryInterface;
+
 class Index extends \Magento\Framework\App\Action\Action
 {
 	protected $logger;
@@ -22,6 +27,11 @@ class Index extends \Magento\Framework\App\Action\Action
 	protected $directory;
 	protected $connection;
 	protected $storeManager;
+	
+	protected $orderShipmentCollection;
+	protected $productFactory;
+	protected $orderFactory;
+	protected $websiteRepository;
 
 	public function __construct(
 		Context $context,
@@ -30,7 +40,11 @@ class Index extends \Magento\Framework\App\Action\Action
 	    UserFactory $userF,
 	    Filesystem $fileS,
 	    ResourceConnection $resourceC,
-	    StoreManagerInterface $storeManagerI
+	    StoreManagerInterface $storeManagerI,
+	    OrderShipmentCollection $orderShipmentC,
+	    ProductFactory $productF,
+	    OrderFactory $orderF,
+	    WebsiteRepositoryInterface $websiteRepositoryI
 	) {
 		$this->logger = $logger;
 		
@@ -43,52 +57,122 @@ class Index extends \Magento\Framework\App\Action\Action
 		
 		$this->storeManager = $storeManagerI;
 		
+		$this->orderShipmentCollection = $orderShipmentC;
+		$this->productFactory = $productF;
+		$this->orderFactory = $orderF;
+		$this->websiteRepository = $websiteRepositoryI;
+		
 		return parent::__construct($context);
 	}
 	
 	public function execute()
 	{
+	    $date = date('Y-m-01', strtotime('now -1 month'));  // now -1 day
+	    $enddate = date('Y-m-t', strtotime('now -1 month'));  // now -1 day
+	    $fromDate = $date.' 00:00:00';
+	    $toDate = $enddate.' 23:59:59';                                                  
 	    
-	    $count_catalogs = 0;
-	    $count_product_catalogs = 0;
-	    $count_engagement_catalog = 0;
-	    $count_custom_catalog = 0;
+	    $filename = '/var/www/magento/var/report/sale-promo-disc__' . $date . '.csv';
 	    
-	    // Date
-	    $start_date = date('Y-m-d',strtotime('now -1 day'));
-	    $end_date = date('Y-m-d',strtotime('now -1 day'));
+	    $stream = $this->directory->openFile($filename, 'w+');
+	    $stream->lock();
 	    
+	    $collection = $this->orderShipmentCollection
+	    ->addAttributeToFilter('created_at', array('from'=>$fromDate, 'to'=>$toDate));
 	    
-	    $query = "
-        	SELECT
-        		first_name,
-        		last_name,
-        		address_1 as address1,
-        		address_2 as address2,
-        		city,
-        		region as state,
-        		postal_code as zip,
-        		country_id as country,
-        		send_product_catalog,
-        		send_engagement_catalog,
-        		send_custom_catalog
-        	FROM
-        		visitor_submissions
-        	WHERE
-        		submitted_at between '".$start_date." 00:00:00' AND '".$end_date." 23:59:59'
-        	AND
-        	(
-        		send_product_catalog = '1'
-        	OR
-        		send_engagement_catalog = '1'
-        	OR
-        		send_custom_catalog = '1'
-        	);
-        ";
-	    
-	    echo $query;die;
+	    $stream->writeCsv(
+	        array(
+	            "Ship/Credit Date",
+	            "Order #",
+	            "SKU",
+	            "Quantity",
+	            "Unit List Price",
+	            "Unit Discounted Price",
+	            "Variance",
+	            "Brand",
+	            "Channel",
+	            "Total Refunded",
+	            "Type"
+	        )
+	    );
 	    
 	    
+	    // get Current Store Name
+	    $stores_list = $this->storeManager->getStores(true, true);
 	    
+	    
+	    foreach ($collection as $shipment) {
+	        $order = $this->orderFactory->create()->load($shipment->getOrderId());
+	        foreach($order->getAllItems() as $item) {
+	            $product = $this->productFactory->create()->load($item->getProductId());
+	            
+	            foreach ($stores_list as $storekey => $storevalue) {
+	                if ($order->getStoreId() == $storevalue->getId()) {
+	                   
+	                    $store_name = $storevalue->getName();
+	                    $website = $this->websiteRepository->getById($storevalue->getWebsiteId());
+	                }
+	            }
+	            
+	         
+	            
+	            if ((($product->getPrice() - $item->getPrice()) > 10) && ($item->getPrice() == $product->getSpecialPrice())) {
+	                $stream->writeCsv(
+	                    array(
+	                        $shipment->getCreatedAt(),
+	                        $order->getIncrementId(),
+	                        $item->getSku(),
+	                        $item->getQtyOrdered(),
+	                        $product->getPrice(),
+	                        $item->getPrice(),
+	                        ($item->getPrice() - $product->getPrice()),
+	                        $website->getName(),
+	                        $store_name,
+	                        (int)$order->getTotalRefunded(),
+	                        "Sale/Promo"
+	                    )
+	                );
+	            }
+	            elseif($item->getOriginalPrice() != $item->getPrice() && $item->getOriginalPrice() > 0) {
+	                $stream->writeCsv(
+	                    array(
+	                        $shipment->getCreatedAt(),
+	                        $order->getIncrementId(),
+	                        $item->getSku(),
+	                        $item->getQtyOrdered(),
+	                        $product->getPrice(),
+	                        $item->getPrice(),
+	                        ($item->getPrice() - $product->getPrice()),
+	                        $website->getName(),
+	                        $store_name,
+	                        (int)$order->getTotalRefunded(),
+	                        "Manual"
+	                    )
+	                );
+	            }
+	        }
+	    }
+	    
+	    
+	    $mail = new \Zend_Mail();
+	    $mail->setBodyHtml("All Sale/Promo/Custom Disc Report - " . $date. " \r\n")
+	    ->setFrom('it@diamondnexus.com', 'Diamond Nexus Reports')
+	    ->setReplyTo('epasek@forevercompanies.com', 'Edie Pasek')
+	    ->addTo('epasek@forevercompanies.com')
+	    ->addTo('bill.tait@forevercompanies.com')
+	    ->addTo('jessica.nelson@diamondnexus.com')
+	    ->addTo('ken.licau@forevercompanies.com')
+	    ->addTo('andrew.roberts@forevercompanies.com')
+	    ->setSubject('Sale/Promo/Custom Disc Report - ' . $date);
+	    
+	    $content = file_get_contents($filename);
+	    $attachment = new \Zend_Mime_Part($content);
+	    $attachment->type = mime_content_type($filename);
+	    $attachment->disposition = \Zend_Mime::DISPOSITION_ATTACHMENT;
+	    $attachment->encoding = \Zend_Mime::ENCODING_BASE64;
+	    $attachment->filename = 'sale-promo-disc_' . $date . '.csv';
+	    
+	    $mail->addAttachment($attachment);
+	    $mail->send();
 	}
 }
