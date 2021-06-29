6a3a3c8db6dea08275ec9f438cb1da88434e2788
Steve Caponetto <steve.caponetto@forevercompanies.com>
2021-06-29 13:51:16 -0500

    create data patch to update gemstone attribute labels

---
+++ app/code/ForeverCompanies/CustomAttributes/Setup/Patch/Data/UpdateGemstoneAttributeLabel.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types=1);
+
+namespace ForeverCompanies\CustomAttributes\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class UpdateGemstoneAttributeLabel implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private ModuleDataSetupInterface $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    private EavSetupFactory $eavSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $attribute = $eavSetup->getAttribute(Product::ENTITY, 'gemstone');
+        if ($attribute) {
+            $eavSetup->updateAttribute(
+                Product::ENTITY,
+                'gemstone',
+                [
+                    'frontend_label' => [
+                        0 => 'Center Stone Size',
+                        1 => null
+                    ]
+                ]
+            );
+        }
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * {}
+     */
+    public static function getVersion(): string
+    {
+        return '0.0.1';
+    }
+}
