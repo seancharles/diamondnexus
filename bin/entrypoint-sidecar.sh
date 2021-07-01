ca7e4f57a1524ddd9ddf177ed2787b47036be995
Edie Pasek <epasek@forevercompanies.com>
2021-07-01 14:54:04 -0500

    Add New Relic Magento logger

--- bin/entrypoint-sidecar.sh
+++ bin/entrypoint-sidecar.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo $NAMESPACE
+sed -i -e 's/"REPLACE_WITH_REAL_KEY"/"fea75357072fdff9844d839de5d86bf4ebca4a6a"/' \
+    -e "s/PHP Application/$NAMESPACE-Magento-Cron/" \
+    -e 's/;newrelic.daemon.app_connect_timeout =.*/newrelic.daemon.app_connect_timeout=15s/' \
+    -e 's/;newrelic.daemon.start_timeout =.*/newrelic.daemon.start_timeout=5s/' \
+    /usr/local/etc/php/conf.d/newrelic.ini
 cd /var/www/magento/
 for x in `php bin/magento indexer:status | grep "Processing" | awk '{print $2}'`
 do 
