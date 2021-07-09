6336d2e6c2a4d13f5d22c9479b2620c6338df6c3
Paul Baum <paul.baum@forevercompanies.com>
2021-07-09 15:57:09 -0500

    Fixin package name typo

---
+++ app/code/ForeverCompanies/ConfigurableOrderItems/Console/Command/FormatOrderItems.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ForeverCompanies\ConfigurableOrderItems\Console\Command;
+
+use Magento\Framework\App\Area;
+use Magento\Framework\App\State;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class SomeCommand
+ */
+class FormatOrderItems extends Command
+{
+    /**
+     * @var string
+     */
+    protected $name = 'forevercompanies:formatconfigorderitems';
+
+    protected $state;
+
+    public function __construct(
+        State $state
+    ) {
+        $this->state = $state;
+
+        parent::__construct($this->name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName($this->name);
+        $this->setDescription("Format magento configurable orders items after migration to M2 format");
+        parent::configure();
+    }
+
+    /**
+     * Execute the command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return null|int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_ADMINHTML);
+
+        echo "Reformat config order items...";
+    }
+}
