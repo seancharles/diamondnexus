586bcf12d0aa24cc2265599ea166e9dac1ce92dc
Steve Caponetto <steve.caponetto@forevercompanies.com>
2021-07-18 20:30:22 -0500

    add file back in

---
+++ app/code/ForeverCompanies/CustomAttributes/Setup/Patch/Data/AddProductTypeProductAttribute.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace ForeverCompanies\CustomAttributes\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Zend_Validate_Exception;
+
+class AddProductTypeProductAttribute implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws LocalizedException
+     * @throws Zend_Validate_Exception
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $attribute = $eavSetup->getAttribute(Product::ENTITY, 'fc_product_type');
+        if ($attribute) {
+            $eavSetup->removeAttribute(
+                Product::ENTITY,
+                'fc_product_type'
+            );
+        }
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'fc_product_type',
+            [
+                'type' => 'int',
+                'label' => 'Product Type',
+                'input' => 'select',
+                'source' => '',
+                'frontend' => '',
+                'required' => true,
+                'backend' => '',
+                'sort_order' => '30',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'default' => null,
+                'visible' => true,
+                'user_defined' => true,
+                'searchable' => true,
+                'filterable' => false,
+                'comparable' => true,
+                'visible_on_front' => true,
+                'unique' => false,
+                'apply_to' => '',
+                'group' => 'General',
+                'used_in_product_listing' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'option' => [
+                    'values' => [
+                        "Bracelet",
+                        "Chain",
+                        "Earring",
+                        "Diamond",
+                        "Gift Card",
+                        "Stone",
+                        "Matched Set",
+                        "Matching Band",
+                        "Necklace",
+                        "Pendant",
+                        "Ring",
+                        "Ring Setting",
+                        "Watch",
+                        "Other"
+                    ]
+                ]
+            ]
+        );
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->removeAttribute(Product::ENTITY, 'fc_product_type');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getVersion()
+    {
+        return '0.0.1';
+    }
+}
