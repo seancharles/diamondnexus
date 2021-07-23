41e2e0dcd03d947d9d2ca7e2667b3ee3f7882ff2
Steve Caponetto <steve.caponetto@forevercompanies.com>
2021-07-23 10:32:42 -0500

    add data patch to update visibility of certified stone attribute

---
+++ app/code/ForeverCompanies/CustomAttributes/Setup/Patch/Data/UpdateCertifiedStoneVisbility.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ForeverCompanies\CustomAttributes\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class UpdateCertifiedStoneVisbility implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private ModuleDataSetupInterface $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    private EavSetupFactory $eavSetupFactory;
+
+    /**
+     * @var string
+     */
+    private string $attribute = 'certified_stone';
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $attribute = $eavSetup->getAttribute(Product::ENTITY, $this->attribute);
+        if ($attribute) {
+            $eavSetup->updateAttribute(
+                Product::ENTITY,
+                $this->attribute,
+                'is_visible_on_front',
+                1
+            );
+        }
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * Module version
+     */
+    public static function getVersion(): string
+    {
+        return '0.0.1';
+    }
+}
