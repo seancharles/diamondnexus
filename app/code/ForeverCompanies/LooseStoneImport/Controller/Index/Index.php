523f9627945fd81a09fe70551414efcec243df29
Zack Bailey <zach.bailey@forevercompanies.com>
2021-08-06 12:52:58 -0500

    ftp logic done

--- app/code/ForeverCompanies/LooseStoneImport/Controller/Index/Index.php
+++ app/code/ForeverCompanies/LooseStoneImport/Controller/Index/Index.php
@@ -4,16 +4,24 @@ namespace ForeverCompanies\LooseStoneImport\Controller\Index;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use ForeverCompanies\LooseStoneImport\Model\StoneImport;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class Index extends Action
 {
     protected $stoneModel;
     
+    protected $scopeConfig;
+    protected $storeScope;
+    
 	public function __construct(
 		Context $context,
-	    StoneImport $stone
+	    StoneImport $stone,
+	    ScopeConfigInterface $scopeC
 	) {
 		$this->stoneModel = $stone;
+		
+		$this->scopeConfig = $scopeC;
+		$this->storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
 		
 		return parent::__construct($context);
 	}
