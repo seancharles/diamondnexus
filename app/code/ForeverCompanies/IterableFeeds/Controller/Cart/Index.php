6130c03c8b37147ed141942c90cdfe7e97b83c35
Paul Baum <paul.baum@forevercompanies.com>
2021-08-09 15:49:18 -0500

    Adding metal tagging to iterable cart feed

--- app/code/ForeverCompanies/IterableFeeds/Controller/Cart/Index.php
+++ app/code/ForeverCompanies/IterableFeeds/Controller/Cart/Index.php
@@ -7,65 +7,101 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\UrlInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Catalog\Model\Product\Attribute\Repository;
 use Psr\Log\LoggerInterface;
 
 class Index extends Action
 {
-    protected $_logger;
-    protected $_jsonResultFactory;
-    protected $_urlInterface;
-    protected $_quoteRepository;
-    protected $_cart;
+    protected $logger;
+    protected $jsonResultFactory;
+    protected $urlInterface;
+    protected $productRepository;
+    protected $quoteRepository;
+    protected $attributeRepository;
 
     public function __construct(
         Context $context,
         LoggerInterface $logger,
         JsonFactory $jsonResultFactory,
         UrlInterface $urlInterface,
-        CartRepositoryInterface $quoteRepository
-    ) {
+        ProductRepositoryInterface $productRepository,
+        CartRepositoryInterface $quoteRepository,
+        Repository $attributeRepository
+    )
+    {
         parent::__construct($context);
 
-        $this->_logger = $logger;
-        $this->_jsonResultFactory = $jsonResultFactory;
-        $this->_urlInterface = $urlInterface;
-        $this->_quoteRepository = $quoteRepository;
+        $this->logger = $logger;
+        $this->jsonResultFactory = $jsonResultFactory;
+        $this->urlInterface = $urlInterface;
+        $this->productRepository = $productRepository;
+        $this->quoteRepository = $quoteRepository;
+        $this->attributeRepository = $attributeRepository;
     }
 
     public function execute()
     {
         $productsArray = [];
+        $metalMap = [];
 
         $request = $this->getRequest()->getParams();
-
-        $result = $this->_jsonResultFactory->create();
+        $result = $this->jsonResultFactory->create();
 
         if (isset($request['qid']) == true) {
-
-            $quoteId = (int)$request['qid'];
+            $quoteId = (int) $request['qid'];
 
             try {
-                $quote = $this->_quoteRepository->get($quoteId);
+                $metalOptions = $this->attributeRepository->get('metal_type')->getOptions();
 
+                foreach($metalOptions as $metalOption) {
+                    $metalMap[$metalOption->getValue()] = strtolower($metalOption->getLabel());
+                }
+
+                $quote = $this->quoteRepository->get($quoteId);
                 $quoteItems = $quote->getAllVisibleItems();
 
                 foreach ($quoteItems as $item) {
+                    $images = [];
+                    $product = $this->productRepository->getById($item->getProductId());
 
-                    $product = $item->getProduct();
+                    $configOptions = $item->getBuyRequest()->getSuperAttribute();
+
+                    if(isset($configOptions[145]) === true) {
+                        $imageGallery = $product->getMediaGalleryImages();
+
+                        foreach($imageGallery as $image) {
+                            $label = strtolower($image->getLabel());
+
+                            if(strlen($label) > 0) {
+                                if(strpos($label,"default") !== false) {
+                                    $metalType = $metalMap[$configOptions[145]];
+
+                                    if(strpos($label, $metalType) !== false) {
+                                        $images[] = $image->getUrl();
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    $images[] = $item->getProduct()->getMediaConfig()->getMediaUrl(
+                        $item->getProduct()->getImage()
+                    );
 
                     $productsArray[] = [
                         'id' => $item->getData('product_id'),
                         'name' => $item->getName(),
                         'price' => $product->getPriceInfo()->getPrice('regular_price')->getAmount()->getBaseAmount(),
                         'special_price' => $product->getFinalPrice(),
-                        'img' => $product->getMediaConfig()->getMediaUrl($product->getImage()),
+                        'img' => $images[0],
                         'url' => $product->getProductUrl(true)
                     ];
                 }
 
                 $result = $result->setData([
-                    'url' => $this->_urlInterface->getUrl('checkout/cart/rebuild', ['id' => $quoteId]),
+                    'url' => $this->urlInterface->getUrl('checkout/cart/rebuild', ['id' => $quoteId]),
                     'products' => $productsArray
                 ]);
 
@@ -75,7 +111,7 @@ class Index extends Action
                     'message' => "Unable to find quote"
                 ]);
 
-                $this->_logger->error(
+                $this->logger->error(
                     'Quote does not exist',
                     [$exception->getMessage()]
                 );
