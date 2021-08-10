8cfaa01082e1428431bebd1fd2900d0f9a72338a
Zack Bailey <zach.bailey@forevercompanies.com>
2021-08-10 12:13:28 -0500

    happy medium bug fix for admin/cart

--- app/code/ForeverCompanies/Profile/Model/ProductRepository.php
+++ app/code/ForeverCompanies/Profile/Model/ProductRepository.php
@@ -9,32 +9,26 @@ class ProductRepository extends OrigProductRepository implements ProductReposito
 {
     public function get($sku, $editMode = false, $storeId = null, $forceReload = false)
     {
-        $cacheKey = $this->getCacheKey([$editMode, $storeId]);
-        
         if (trim($sku) == "") {
             return null;
         }
         
-        if ($cachedProduct === null || $forceReload) {
-            $product = $this->productFactory->create();
-            
-            $productId = $this->resourceModel->getIdBySku($sku);
-            if (!$productId) {
-                throw new NoSuchEntityException(
-                    __("The product that was requested doesn't exist. Verify the product and try again.")
-                    );
-            }
-            if ($editMode) {
-                $product->setData('_edit_mode', true);
-            }
-            if ($storeId !== null) {
-                $product->setData('store_id', $storeId);
-            }
-            $product->load($productId);
-            $this->cacheProduct($cacheKey, $product);
-            $cachedProduct = $product;
-        }
+        $product = $this->productFactory->create();
         
-        return $cachedProduct;
+        $productId = $this->resourceModel->getIdBySku($sku);
+        if (!$productId) {
+            throw new NoSuchEntityException(
+                __("The product that was requested doesn't exist. Verify the product and try again.")
+                );
+        }
+        if ($editMode) {
+            $product->setData('_edit_mode', true);
+        }
+        if ($storeId !== null) {
+            $product->setData('store_id', $storeId);
+        }
+        $product->load($productId);
+        
+        return $product;
     }
 }
