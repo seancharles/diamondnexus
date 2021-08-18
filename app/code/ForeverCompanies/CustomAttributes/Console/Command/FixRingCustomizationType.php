7cab314394af0959275e2d91c0fead4e2c6f565c
Zack Bailey <zach.bailey@forevercompanies.com>
2021-08-18 12:11:16 -0500

    description and code cleanup

--- app/code/ForeverCompanies/CustomAttributes/Console/Command/FixRingCustomizationType.php
+++ app/code/ForeverCompanies/CustomAttributes/Console/Command/FixRingCustomizationType.php
@@ -7,12 +7,7 @@ declare(strict_types=1);
 
 namespace ForeverCompanies\CustomAttributes\Console\Command;
 
-use Magento\CatalogStaging\Model\ResourceModel\ProductSequence;
-use Magento\Framework\App\Area;
-use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\App\State;
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Setup\Patch\PatchHistory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,7 +17,6 @@ use Magento\Catalog\Model\Product\Option;
 
 class FixRingCustomizationType extends Command
 {
-
     /**
      * @var string
      */
@@ -32,11 +26,6 @@ class FixRingCustomizationType extends Command
      * @var State
      */
     protected $state;
-
-    /**
-     * @var ResourceConnection
-     */
-    protected $resource;
     
     protected $productColl;
     protected $productOptionModel;
@@ -44,19 +33,16 @@ class FixRingCustomizationType extends Command
     /**
      * RefreshPatchList constructor.
      * @param State $state
-     * @param ResourceConnection $resourceConnection
      * @param string|null $name
      */
     public function __construct(
         State $state,
-        ResourceConnection $resourceConnection,
         CollectionFactory $collectionFac,
         Option $opt,
         string $name = null
     ) {
         parent::__construct($name);
         $this->state = $state;
-        $this->resource = $resourceConnection;
         
         $this->productColl = $collectionFac->create();
         $this->productOptionModel = $opt;
@@ -91,21 +77,12 @@ class FixRingCustomizationType extends Command
     }
 
     /**
-     * @param string $id
-     */
-    private function addSequence($id)
-    {
-        $tableName = $this->resource->getTableName(ProductSequence::SEQUENCE_TABLE);
-        $this->resource->getConnection()->insertOnDuplicate($tableName, ['sequence_value' => $id]);
-    }
-
-    /**
      * {@inheritdoc}
      */
     protected function configure()
     {
         $this->setName($this->name);
-        $this->setDescription("Refresh setup patch list for correct migrate attributes from 2.3.6 to 2.4.1");
+        $this->setDescription("Sets Customizable Options -> Customization Type to 'Ring Size' for Products in Rings Attribute Set");
         parent::configure();
     }
 }
