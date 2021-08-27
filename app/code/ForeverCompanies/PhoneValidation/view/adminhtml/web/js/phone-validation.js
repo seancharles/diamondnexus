8dd04c74f760b914658917d4cb68f15e599e3f71
Zack Bailey <zach.bailey@forevercompanies.com>
2021-08-26 19:26:06 -0500

    phone validation

--- app/code/ForeverCompanies/PhoneValidation/view/adminhtml/web/js/phone-validation.js
+++ app/code/ForeverCompanies/PhoneValidation/view/adminhtml/web/js/phone-validation.js
@@ -1,13 +1,13 @@
 require(['jquery', 'jquery/ui', 'mage/validation/validation'], function($, ui, validation){ 
 	
 	$( document ).ready(function() {
-	    $("#order-billing_address_telephone").addClass("validate-number");
-		$("#order-shipping_address_telephone").addClass("validate-number");
+	    $("#order-billing_address_telephone").addClass("validate-phoneStrict");
+		$("#order-shipping_address_telephone").addClass("validate-phoneStrict");
 	});
 	
 	$( document ).ajaxComplete(function( event,request, settings ) {
-		$("#order-billing_address_telephone").addClass("validate-number");
-		$("#order-shipping_address_telephone").addClass("validate-number");
+		$("#order-billing_address_telephone").addClass("validate-phoneStrict");
+		$("#order-shipping_address_telephone").addClass("validate-phoneStrict");
 	});
 
 });
