c8c039aceb68020d062e8057c4cbfa0be37d7285
Paul Baum <paul.baum@forevercompanies.com>
2021-09-09 15:08:38 -0500

    Adding 32 digit sku fix for fishbowl API

---
+++ app/code/ForeverCompanies/CustomApi/Plugin/OrderItemSku.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace ForeverCompanies\CustomApi\Plugin;
+
+use Magento\Sales\Api\Data\OrderItemInterface;
+use Magento\Sales\Api\OrderItemRepositoryInterface;
+use Magento\Sales\Api\Data\OrderItemSearchResultInterface;
+
+class OrderItemSku
+{
+    /**
+     *
+     * @param OrderItemRepositoryInterface $subject
+     * @param OrderItemInterface $orderItem
+     *
+     * @return OrderItemInterface
+     */
+    public function afterGet(
+        OrderItemRepositoryInterface $subject,
+        OrderItemInterface $orderItem
+    ) {
+        $sku = str_replace("-", "", $orderItem->getSku());
+
+        $prefix = strtoupper(substr($sku,0,4));
+        $metal = strtoupper(substr($sku,24,2));
+        $color = strtoupper(substr($sku,15,2));
+        $line = strtoupper(substr($sku,4,2));
+
+        if(
+            ( $prefix == 'MRTN' ) ||
+            ( $prefix == 'MRTT' ) ||
+            ( $prefix == 'MRWB' ) ||
+            ( $prefix == 'LRRH' ) ||
+            ( $prefix == 'LRWB' ) ||
+            ( $metal == 'CO' ) ||
+            ( $metal == 'BZ' ) ||
+            ( $metal == 'DM' ) ||
+            ( $metal == 'TT' ) ||
+            ( $prefix == 'LRRH' && $color == 'TZ') ||
+            ( $prefix == 'LRRH' && $metal == 'LP') ||
+            ( $prefix == 'LREB') ||
+            ( $prefix == 'LREN') ||
+            ( $line == 'RS')
+        ) {
+            $sku = substr($sku,0,32);
+        } else {
+            $sku = substr($sku,0,28);
+        }
+
+        $orderItem->setSku($sku);
+
+        return $orderItem;
+    }
+
+    /**
+     *
+     * @param OrderItemRepositoryInterface $subject
+     * @param OrderItemSearchResultInterface $searchResult
+     *
+     * @return OrderItemSearchResultInterface
+     */
+    public function afterGetList(OrderItemRepositoryInterface $subject, OrderItemSearchResultInterface $searchResult)
+    {
+        $orderItems = $searchResult->getItems();
+
+        foreach ($orderItems as &$item) {
+            $sku = str_replace("-", "", $item->getSku());
+
+            $prefix = strtoupper(substr($sku,0,4));
+            $metal = strtoupper(substr($sku,24,2));
+            $color = strtoupper(substr($sku,15,2));
+            $line = strtoupper(substr($sku,4,2));
+
+            if(
+                ( $prefix == 'MRTN' ) ||
+                ( $prefix == 'MRTT' ) ||
+                ( $prefix == 'MRWB' ) ||
+                ( $prefix == 'LRRH' ) ||
+                ( $prefix == 'LRWB' ) ||
+                ( $metal == 'CO' ) ||
+                ( $metal == 'BZ' ) ||
+                ( $metal == 'DM' ) ||
+                ( $metal == 'TT' ) ||
+                ( $prefix == 'LRRH' && $color == 'TZ') ||
+                ( $prefix == 'LRRH' && $metal == 'LP') ||
+                ( $prefix == 'LREB') ||
+                ( $prefix == 'LREN') ||
+                ( $line == 'RS')
+            ) {
+                $sku = substr($sku,0,32);
+            } else {
+                $sku = substr($sku,0,28);
+            }
+
+            $item->setSku($sku);
+        }
+
+        return $searchResult;
+    }
+}
