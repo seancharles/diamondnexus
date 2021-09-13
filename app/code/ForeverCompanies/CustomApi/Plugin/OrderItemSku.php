a33d92a669f365c1f427ec426e2654c69d5c9494
Paul Baum <paul.baum@forevercompanies.com>
2021-09-13 13:45:29 -0500

    Code cleanup

--- app/code/ForeverCompanies/CustomApi/Plugin/OrderItemSku.php
+++ app/code/ForeverCompanies/CustomApi/Plugin/OrderItemSku.php
@@ -19,7 +19,31 @@ class OrderItemSku
         OrderItemRepositoryInterface $subject,
         OrderItemInterface $orderItem
     ) {
-        $sku = str_replace("-", "", $orderItem->getSku());
+        $orderItem->setSku($this->transformFishbowlItemSku($orderItem));
+
+        return $orderItem;
+    }
+
+    /**
+     *
+     * @param OrderItemRepositoryInterface $subject
+     * @param OrderItemSearchResultInterface $searchResult
+     *
+     * @return OrderItemSearchResultInterface
+     */
+    public function afterGetList(OrderItemRepositoryInterface $subject, OrderItemSearchResultInterface $searchResult)
+    {
+        $orderItems = $searchResult->getItems();
+
+        foreach ($orderItems as &$item) {
+            $item->setSku($this->transformFishbowlItemSku($item));
+        }
+
+        return $searchResult;
+    }
+
+    protected function transformFishbowlItemSku($item)  {
+        $sku = str_replace("-", "", $item->getSku());
 
         $prefix = strtoupper(substr($sku,0,4));
         $metal = strtoupper(substr($sku,24,2));
@@ -42,49 +66,6 @@ class OrderItemSku
             $sku = substr($sku,0,28);
         }
 
-        $orderItem->setSku($sku);
-
-        return $orderItem;
-    }
-
-    /**
-     *
-     * @param OrderItemRepositoryInterface $subject
-     * @param OrderItemSearchResultInterface $searchResult
-     *
-     * @return OrderItemSearchResultInterface
-     */
-    public function afterGetList(OrderItemRepositoryInterface $subject, OrderItemSearchResultInterface $searchResult)
-    {
-        $orderItems = $searchResult->getItems();
-
-        foreach ($orderItems as &$item) {
-            $sku = str_replace("-", "", $item->getSku());
-
-            $prefix = strtoupper(substr($sku,0,4));
-            $metal = strtoupper(substr($sku,24,2));
-            $line = strtoupper(substr($sku,4,2));
-
-            if(
-                ( $prefix == 'MRWB' ) ||
-                ( $prefix == 'MRFS' ) ||
-                ( $prefix == 'LREN' &&  $line == 'RS') ||
-                ( $prefix == 'LREB' ) ||
-                ( $prefix == 'MRTT' ) ||
-                ( $prefix == 'MRWB' ) ||
-                ( $metal == 'CO' ) ||
-                ( $metal == 'TT' ) ||
-                ( $prefix == 'MRTN') ||
-                ( $prefix == 'LRRH' && $metal == 'LP' )
-            ) {
-                $sku = substr($sku,0,32);
-            } else {
-                $sku = substr($sku,0,28);
-            }
-
-            $item->setSku($sku);
-        }
-
-        return $searchResult;
+        return $sku;
     }
 }
