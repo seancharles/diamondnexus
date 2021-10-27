529ff435554b45d810464163501e7a5525fee38e
Paul Baum <paul.baum@forevercompanies.com>
2021-10-27 22:55:16 +0000

    Updating record type id values for SF

--- app/code/ForeverCompanies/Salesforce/Helper/Sync.php
+++ app/code/ForeverCompanies/Salesforce/Helper/Sync.php
@@ -368,15 +368,15 @@ class Sync extends AbstractHelper
                         switch($leadModel->getWebsiteId()) {
                             case "1":
                                 // DN_Lead
-                                $recordTypeId = "0121J000001DeHDQA0";
+                                $recordTypeId = "0120v000000X2vXAAS";
                                 break;
                             case "2":
                                 // FA_Lead
-                                $recordTypeId = "0121J000001DeHNQA0";
+                                $recordTypeId = "0120v000000X2vcAAC";
                                 break;
                             case "3":
                                 // X12Fifteen_Lead
-                                $recordTypeId = "0121J000001DeHNQA0";
+                                $recordTypeId = "0120v000000X2vSAAS";
                                 break;
                             default:
                                 $recordTypeId = '012o0000000y9txAAA';
